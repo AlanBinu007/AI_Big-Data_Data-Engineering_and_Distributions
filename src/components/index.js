9b84e698f409a63305cf926487df0dd8b870a54e
Alan Binu <65335042+AlanBinu007@users.noreply.github.com>
2021-04-19 10:23:00 +0530

    E-Commerce-App

---
+++ src/components/index.js
@@ -0,0 +1,5 @@
+export { default as Navbar } from './Navbar/Navbar';
+export { default as Products } from './Products/Products';
+export { default as Cart } from './Cart/Cart';
+export { default as Checkout } from './CheckoutForm/Checkout/Checkout';
+
