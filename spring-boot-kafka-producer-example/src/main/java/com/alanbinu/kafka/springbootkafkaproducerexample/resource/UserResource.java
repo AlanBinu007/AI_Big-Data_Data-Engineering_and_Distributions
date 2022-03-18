642b2bc2600ea100a43b69e88269bee518455bd4
Alan Binu <alanbinu5@gmail.com>
2022-03-18 11:10:09 +0530

    spring-boot-kafka-producer-example
    spring-boot-kafka-producer-example

---
+++ spring-boot-kafka-producer-example/src/main/java/com/alanbinu/kafka/springbootkafkaproducerexample/resource/UserResource.java
@@ -0,0 +1,27 @@
+package com.alanbinu.kafka.springbootkafkaproducerexample.resource;
+
+import com.alanbinu.kafka.springbootkafkaproducerexample.model.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("kafka")
+public class UserResource {
+
+    @Autowired
+    private KafkaTemplate<String, User> kafkaTemplate;
+
+    private static final String TOPIC = "Kafka_Example";
+
+    @GetMapping("/publish/{name}")
+    public String post(@PathVariable("name") final String name) {
+
+        kafkaTemplate.send(TOPIC, new User(name, "Technology", 12000L));
+
+        return "Published successfully";
+    }
+}
